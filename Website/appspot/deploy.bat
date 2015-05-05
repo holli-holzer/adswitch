af59fcb9901d177979f75b1f9207f10554b63b5a
holli-holzer <holli.holzer@gmail.com>
2015-05-05 09:32:03 +0200

    Delete deploy.bat

--- Website/appspot/deploy.bat
+++
@@ -1,2 +0,0 @@
-echo i_want_my_mail|d:\python\python.exe "C:\\Program Files (x86)\\Google\\google_appengine\\appcfg.py" --no_cookies --email=holli.holzer@googlemail.com --passin update d:\Projekte\ADSwitch\Website\appspot
-pause
